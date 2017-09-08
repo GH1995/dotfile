6b88fe20f7ce1beca40b61949c4983b5262df335
hxs <tulingjiaoyu@gmail.com>
2017-09-08 18:17:32 +0800

    mod install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-ln latexmkrc ~/.latexmkrc
-ln npmrc ~/.npmrc
-ln pip.conf ~/.pip/pip.conf
-ln zshrc ~/.zshrc
+if [ ! -e ~/.latexmkrc ]
+then
+    ln latexmkrc ~/.latexmkrc
+fi
+
+if [ ! -e ~/.npmrc ]
+then
+    ln npmrc ~/.npmrc
+fi
+
+if [ -e ~/.pip/pip.conf ]
+then
+    mkdir -p ~/.pip
+    ln pip.conf ~/.pip/pip.conf
+fi
+
+if [ -e ~/.zshrc ]
+then
+    ln zshrc ~/.zshrc
+fi
