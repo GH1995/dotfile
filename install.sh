85ba93e33528810f6c5a100915e2da0d322600a4
GH1995 <tulingjiaoyu@gmai.com>
2017-10-15 13:19:43 +0800

    mod install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,13 @@ rm -rf ~/.npmrc
 ln ~/dotfile/npmrc ~/.npmrc
 
 rm -rf ~/.pip/pip.conf
-mkdir -p ~/.pip
-ln ~/dotfile/pip.conf ~/.pip/pip.conf
+mkdir -p ~/.pip && ln ~/dotfile/pip.conf ~/.pip/pip.conf
 
 rm -rf ~/.zshrc
 ln ~/dotfile/zshrc ~/.zshrc
 
 rm -rf ~/.tmux*
-cd
-git clone git@github.com:gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-ln ~/dotfile/tmux.conf.local ~/.tmux.conf.local
+cd && \
+    git clone git@github.com:gpakosz/.tmux.git && \
+    ln -s -f .tmux/.tmux.conf && \
+    ln ~/dotfile/tmux.conf.local ~/.tmux.conf.local
