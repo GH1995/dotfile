e0697cdb22e0857216c16988c1b201848905a511
GH1995 <tulingjiaoyu@gmail.com>
2017-12-20 20:02:00 +0800

    add nothing

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ mkdir -p ~/.pip && ln ~/dotfile/pip.conf ~/.pip/pip.conf
 rm -rf ~/.zshrc
 ln ~/dotfile/zshrc ~/.zshrc
 
-if [ ! -d "~/.tmux" ]; then
+if [ ! -d ~/.tmux ]; then
     git clone git@github.com:gpakosz/.tmux.git && \
     ln -s -f .tmux/.tmux.conf && \
     ln ~/dotfile/tmux.conf.local ~/.tmux.conf.local
