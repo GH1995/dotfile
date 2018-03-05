ad37df332f549d6008ed9997098d22138404cc36
GH1995 <tulingjiaoyu@gmail.com>
2018-03-05 12:35:33 +0800

    dotfile => .dotfile

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 rm -rf ~/.latexmkrc
-ln ~/dotfile/latexmkrc ~/.latexmkrc
+ln ~/.dotfile/latexmkrc ~/.latexmkrc
 
 rm -rf ~/.npmrc
-ln ~/dotfile/npmrc ~/.npmrc
+ln ~/.dotfile/npmrc ~/.npmrc
 
 rm -rf ~/.pip/pip.conf
-mkdir -p ~/.pip && ln ~/dotfile/pip.conf ~/.pip/pip.conf
+mkdir -p ~/.pip && ln ~/.dotfile/pip.conf ~/.pip/pip.conf
 
 rm -rf ~/.zshrc
-ln ~/dotfile/zshrc ~/.zshrc
+ln ~/.dotfile/zshrc ~/.zshrc
 
 rm -rf ~/.tmux ~/.tmux.conf ~/.tmux.conf.local
 cd && git clone git@github.com:gpakosz/.tmux.git && \
